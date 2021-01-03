2747bc3a9f84fc5f9a20c01e024501e588d69c23
BlockChanZJ <386636627@qq.com>
2021-01-03 16:57:02 +0800

    update bomblab-phase_2-4

--- lab/bomblab/my.cpp
+++ lab/bomblab/my.cpp
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-int read_six_numbers(int a[]) {
-	scanf ("%d%d%d%d%d%d",a,a+1,a+2,a+3,a+4,a+5);
-}
-
 int main () {
-	int a[6] = {0};
-	puts("");
-	read_six_numbers(a);
-	puts("");
-	for (int i = 0; i < 6; ++i) 
-		printf ("%d\n",a[i]);
 	return 0;
 }
