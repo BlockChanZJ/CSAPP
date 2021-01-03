fb7215323e3c5e6b53ea9090ae4a931e5ca2e29c
BlockChanZJ <386636627@qq.com>
2021-01-03 19:52:11 +0800

    update bomblab-phase_5

--- lab/bomblab/my.cpp
+++ lab/bomblab/my.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+struct P1 {
+	int a;
+	double b;
+	char c;
+};
+struct P2 {
+	double b;
+	int a;
+	char c;
+};
+
 int main () {
+	printf ("%d %d\n",sizeof(P1), sizeof(P2));
 	return 0;
 }
